ec05bb734c4622fa47eac52dd8768cfd5f061bfd
rkkansy <robert.konrad.kansy@gmail.com>
2024-05-16 15:52:16 +0200

    fixed parameters

--- param.py
+++ param.py
@@ -152,7 +152,7 @@ def process_args():
     parser.add_argument("--dynamics_path", default="data/dynamics/", type=str, help="Path to save the dynamics data.")
     parser.add_argument("--first_dynamics_ckpt", default=0, type=int, help="The first checkpoint that should be evaluated for training dynamics.")
 
-    parser.add_argument("--data_partition", nargs='*', default=['none'], type=str,
+    parser.add_argument("--data_partition", default='none', type=str,
                         choices=['easy', 'ambiguous', 'hard', 'rand', 'none'],
                         help="Which part of the data should be used for training. Requires training dynamics to be computed."
     )
