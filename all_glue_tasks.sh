e31328a28dffed96bcb75119b4e06cb3aff936f5
rkkansy <robert.konrad.kansy@gmail.com>
2024-08-13 16:32:52 +0200

    Update GLUE eval script

--- all_glue_tasks.sh
+++ all_glue_tasks.sh
@@ -1,35 +1,27 @@
-# Activate conda env
-# ...
-
 logger = log
-# Specify the learning rates, batch sizes, and task names you want to use
 learning_rates=("4e-5")
 batch_sizes=(16)
+seeds=(42)
 task_names=("mrpc" "cola" "rte" "stsb")
 long_task_names=("qqp" "qnli" "mnli" "sst2")
-# Path to the Python script
-python_script=run_glue_n.py
+python_script=run_glue.py
 model_path=""
-model_name=""
-# Common parameters
-common_params="--model_name_or_path ${model_path} --logging_steps 100 --max_seq_length 128 --overwrite_output_dir --do_train --do_eval --evaluation_strategy steps --gradient_accumulation_steps 1 --weight_decay 0.01 --max_grad_norm 1.0 --num_train_epochs 5 --lr_scheduler_type cosine --warmup_ratio 0.05"
 
-# Iterate over learning rates, batch sizes, and task names
-for lr in "${learning_rates[@]}"; do
-    for batch_size in "${batch_sizes[@]}"; do
-        for task_name in "${task_names[@]}"; do
-            # Create output directory based on task_name, learning_rate, and batch_size
-            output_dir="eval/${model_name}${task_name}_lr${lr}_batch${batch_size}"
+for seed in "${seeds[@]}"; do
 
-            # Execute the Python script with the current parameters
-            python $python_script $common_params --eval_steps 50 --task_name $task_name --learning_rate $lr --per_device_train_batch_size $batch_size --per_device_eval_batch_size $batch_size --output_dir $output_dir 
-        done
-        for task_name in "${long_task_names[@]}"; do
-            # Create output directory based on task_name, learning_rate, and batch_size
-            output_dir="eval/${model_name}${task_name}_lr${lr}_batch${batch_size}"
+    model_name="-${seed}-"
+    common_params="--model_name_or_path ${model_path} --seed ${seed} --logging_steps 100 --max_seq_length 128 --overwrite_output_dir --do_train --do_eval --evaluation_strategy steps --gradient_accumulation_steps 1 --weight_decay 0.01 --max_grad_norm 1.0 --num_train_epochs 5 --lr_scheduler_type cosine --warmup_ratio 0.1"
 
-            # Execute the Python script with the current parameters
-            python $python_script $common_params --eval_steps 1000 --task_name $task_name --learning_rate $lr --per_device_train_batch_size $batch_size --per_device_eval_batch_size $batch_size --output_dir $output_dir 
+    for lr in "${learning_rates[@]}"; do
+        for batch_size in "${batch_sizes[@]}"; do
+            for task_name in "${task_names[@]}"; do
+                output_dir="eval/${model_name}${task_name}_lr${lr}_batch${batch_size}"
+                python3 $python_script $common_params --eval_steps 50 --task_name $task_name --learning_rate $lr --per_device_train_batch_size $batch_size --per_device_eval_batch_size $batch_size --output_dir $output_dir 
+            done
+            for task_name in "${long_task_names[@]}"; do
+                output_dir="eval/${model_name}${task_name}_lr${lr}_batch${batch_size}"
+                python3 $python_script $common_params --eval_steps 5000 --task_name $task_name --learning_rate $lr --per_device_train_batch_size $batch_size --per_device_eval_batch_size $batch_size --output_dir $output_dir 
+            done
         done
     done
 done
